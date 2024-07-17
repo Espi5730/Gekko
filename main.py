63bc15e335ad62e4f4b9d95a62958fc1358faa08
adamabdicode <adamabdi2k@gmail.com>
2024-07-17 18:15:36 +0000

    setting up chatbot

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import os 
+import openai
+from openai import OpenAI
 
 app = Flask(__name__)
 
@@ -9,6 +12,25 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+def chatBot():
+    my_api_key=('OPENAI_KEY')
+
+    client=OpenAI(
+        api_key=my_api_key
+        )
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a financial advisor or stock expert that gives advice to college students who are curious about anything related to stocks and financing for stocks"},
+        {"role": "user", "content": "What are the advantages of pair programming?"}
+    ]
+ )
+    print(completion.choices[0].message.content) 
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
