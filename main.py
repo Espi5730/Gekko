825164a0d2a620987fdc1fd4392f0808c90b40d4
adamabdicode <adamabdi2k@gmail.com>
2024-07-18 18:16:02 +0000

    setting up the database

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
@@ -18,5 +19,17 @@ def portfolio():
 def market():
     return render_template('market.html')
 
+#Setting up the database
+conn=sqlite3.connect('personal-portfolio.db',check_same_thread=False)
+c=conn.cursor()
+c.execute('''
+    CREATE TABLE IF NOT EXISTS portfolio (
+    id Integer PRIMARY KEY,
+    name TEXT NOT NULL,
+    price Integer,
+    changeInPrice Integer
+    )
+''')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0")
