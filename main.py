db59c19f7f94a323b27527c4dfbf798e42b53913
adamabdicode <adamabdi2k@gmail.com>
2024-07-18 20:18:05 +0000

    adding chatbot to backend

--- main.py
+++ main.py
@@ -177,5 +177,21 @@ def addDatabase(name,price,changeInPrice):
     ''',(name,price,changeInPrice)
     )
 
+#Setting up chatBot
+def chatBot():
+    my_api_key=('OPENAI_KEY')
+
+    client=OpenAI(
+        api_key=my_api_key
+        )
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a financial advisor or stock expert that gives advice to college students who are curious about anything related to stocks and financing for stocks"},
+        {"role": "user", "content": "What are the advantages of pair programming?"}
+    ]
+ )
+    print(completion.choices[0].message.content) 
+
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0")
