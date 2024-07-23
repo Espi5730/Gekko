2a60671ec05d5ab39b39b3d825105b9496ecff57
Brice Patchou <108960259+Brice12347@users.noreply.github.com>
2024-07-23 13:31:30 -0400

    GEEKO

--- main.py
+++ main.py
@@ -673,4 +673,4 @@ def chat_bot():
         return jsonify({'response': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0", port=7000)
+    app.run(debug=True,host="0.0.0.0", port=5001)
